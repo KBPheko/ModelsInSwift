8676d38781fa12b0205da62830eb02f8d6c7ffd3
KBPheko <@J8riW2aG*>
2023-05-24 14:52:37 +0200

    first commit

---
+++ Models/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Models
+//
+//  Created by DA MAC M1 151 on 2023/05/24.
+//
+
+import Foundation
---
+++ Models/ToDoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoTableViewCell.swift
+//  Models
+//
+//  Created by DA MAC M1 151 on 2023/05/24.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
